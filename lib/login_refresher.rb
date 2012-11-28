c1ef57742e6c758f4ec5a6fd75c4c5bf2963794d
imjoey <majunjiev@gmail.com>
2012-11-28 20:17:58 +0800

    simplify the logic of maintaining the logined in status

--- lib/login_refresher.rb
+++ lib/login_refresher.rb
@@ -2,11 +2,11 @@ require 'selenium-webdriver'
 
 # initialize the global variables
 driver = Selenium::WebDriver.for :firefox
-wait = Selenium::WebDriver::Wait.new(:timeout => 60)
 
 loop do
   # then visit the /index to refresh code
   driver.get 'http://localhost:4567/connect'
+
   sleep 36000
 end
 
