76e848f2cc20d82eff670df7ca40e32079969d9f
imjoey <majunjiev@gmail.com>
2012-11-28 15:34:14 +0800

    simplify the login status refresher

--- lib/login_refresher.rb
+++ lib/login_refresher.rb
@@ -1,32 +1,13 @@
-# encoding: utf-8
 require 'selenium-webdriver'
 
 # initialize the global variables
 driver = Selenium::WebDriver.for :firefox
 wait = Selenium::WebDriver::Wait.new(:timeout => 60)
 
-# first open the pre-logined weibo account
-driver.get 'http://weibo.com'
-wait.until { driver.title.start_with? '新浪微博' }
-
 loop do
   # then visit the /index to refresh code
   driver.get 'http://localhost:4567/connect'
-=begin
-  # fill in the user id input-field
-  element = driver.find_element :name => 'userId'
-  element.clear
-  element.send_keys 'majunjiev@gmail.com'
-
-  # fill in the password input-field
-  element2 = driver.find_element :name => 'passwd'
-  element2.clear
-  element2.send_keys 'joeyyeoj321'
-  element2.submit
-=end
-
   sleep 36000
-
 end
 
 driver.quit
