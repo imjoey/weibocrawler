337b8456693c79fd5814bf5d49a0c190afc22f80
imjoey <majunjiev@gmail.com>
2012-12-04 12:09:08 +0800

    complete the lgin refresher task

--- lib/login_refresher.rb
+++ lib/login_refresher.rb
@@ -2,12 +2,24 @@ require 'selenium-webdriver'
 
 # initialize the global variables
 driver = Selenium::WebDriver.for :firefox
+wait = Selenium::WebDriver::Wait.new(:timeout => 60)
 
+count = 100
 loop do
-  # then visit the /index to refresh code
-  driver.get 'http://localhost:4567/connect'
+  # then visit the /connect to refresh code
+  if count == 100
+    driver.get 'http://localhost:4567/connect'
+    wait.until { driver.title.start_with? 'weibo oauth2' }
+    count = 0
+  end
 
-  sleep 36000
+  # load statuses into mongodb
+  driver.get 'http://localhost:4567/statuses'
+  wait.until { driver.body.start_with? 'Add' }
+
+  # every 6 minues do the statuses loading
+  sleep 360
+  count = count + 1
 end
 
 driver.quit
